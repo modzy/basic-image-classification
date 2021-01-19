1a1b294e2d41c3dead39d6cb8c8628e5940283bc
Bradley Munday <bmunda@bigbet2-dgx.lnx.boozallencsn.com>
2021-01-19 11:56:36 -0500

    Modified weights script

--- download_weights.py
+++ download_weights.py
@@ -1,9 +1,16 @@
 import os
 import torch
 from torchvision import models, transforms
+import sys
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 WEIGHTS_DIR = os.path.join(ROOT, 'weights')
 
+if not os.path.isdir(WEIGHTS_DIR):
+    os.mkdir(WEIGHTS_DIR)
+
 resnet_model = models.resnet101(pretrained=True)
 torch.save(resnet_model.state_dict(), os.path.join(WEIGHTS_DIR, 'resnet101_weights.pth'))
+
+
+
